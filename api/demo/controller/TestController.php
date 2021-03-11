fe18af77db8498bdac429d52a9380e23e853017f
zhangpai <394980922@qq.com>
2021-03-11 18:07:13 +0800

    文件同步

--- api/demo/controller/TestController.php
+++ api/demo/controller/TestController.php
@@ -22,11 +22,7 @@ class TestController extends RestBaseController
 
     public function test()
     {
-        $http = new swoole_http_server('0.0.0.0', 80, SWOOLE_BASE);
-        $http->on('request', function(swoole_http_request $req, swoole_http_response $res) use($http) {
-            $res->write("hello world");
-            $res->end();
-        });
+        echo 'hhh';
     }
 
     //生成门店月统计表
