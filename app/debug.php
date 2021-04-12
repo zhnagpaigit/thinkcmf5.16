0df36045dce25394a1c3bb2a1ce26dc2fb4dd2c1
zhangpai <394980922@qq.com>
2021-04-12 10:49:32 +0800

    thinkcmf

---
+++ app/debug.php
@@ -0,0 +1,23 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2017 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: Dean <zxxjjforever@163.com>
+// +----------------------------------------------------------------------
+
+return [
+    // 应用调试模式
+    'APP_DEBUG' =>true ,
+//    // 应用Trace
+//    'app_trace' => true,
+//    'LOG_RECORD' => false,
+   'LOG_EXCEPTION_RECORD' => true,// 是否记录异常信息日志
+//    'APP_DEBUG' =>false ,
+    // 应用Trace
+//    'app_trace' => false,
+
+];
