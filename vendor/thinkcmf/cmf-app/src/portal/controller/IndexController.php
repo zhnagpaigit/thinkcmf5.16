163b29397eb09297dc886c1458bb598ea7d2aad9
zhangpai <394980922@qq.com>
2021-04-13 15:55:27 +0800

    token

--- vendor/thinkcmf/cmf-app/src/portal/controller/IndexController.php
+++ vendor/thinkcmf/cmf-app/src/portal/controller/IndexController.php
@@ -18,7 +18,27 @@ class IndexController extends HomeBaseController
     // 首页
     public function index()
     {
-        return $this->fetch(":index");
+//        return $this->fetch(":index");
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = 'cozy';
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+        file_put_contents('1.txt',json_encode($_REQUEST).PHP_EOL, FILE_APPEND);
+        if( $tmpStr == $signature ){
+            header('content-type:text');
+            echo $_GET['echostr'];
+            file_put_contents('1.txt',"success".PHP_EOL, FILE_APPEND);
+            return true;
+        }else{
+            echo "failure";
+            file_put_contents('1.txt',"failure".PHP_EOL, FILE_APPEND);
+            return false;
+        }
     }
     public function wxToken()
     {
