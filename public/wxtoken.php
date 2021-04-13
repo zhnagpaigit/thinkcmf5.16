216dd9cd9b9089b4f16ac2c4be7c4219329e89cd
zhangpai <394980922@qq.com>
2021-04-13 16:02:12 +0800

    thinkcmf

--- public/wxtoken.php
+++ public/wxtoken.php
@@ -8,14 +8,14 @@ $tmpArr = array($token, $timestamp, $nonce);
 sort($tmpArr, SORT_STRING);
 $tmpStr = implode( $tmpArr );
 $tmpStr = sha1( $tmpStr );
-file_put_contents('1.txt',json_encode($_REQUEST).PHP_EOL, FILE_APPEND);
+file_put_contents('../data/runtime/log/1.txt',json_encode($_REQUEST).PHP_EOL, FILE_APPEND);
 if( $tmpStr == $signature ){
     header('content-type:text');
     echo $_GET['echostr'];
-    file_put_contents('1.txt',"success".PHP_EOL, FILE_APPEND);
+    file_put_contents('../data/runtime/log/1.txt',"success".PHP_EOL, FILE_APPEND);
     return true;
 }else{
     echo "failure";
-    file_put_contents('1.txt',"failure".PHP_EOL, FILE_APPEND);
+    file_put_contents('../data/runtime/log/1.txt',"failure".PHP_EOL, FILE_APPEND);
     return false;
 }
