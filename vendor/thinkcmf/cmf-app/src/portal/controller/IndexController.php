1463a5ccccbc3de8892f89c3142e8a1dc183ecff
zhangpai <394980922@qq.com>
2021-04-14 11:26:39 +0800

    thinkcmf

--- vendor/thinkcmf/cmf-app/src/portal/controller/IndexController.php
+++ vendor/thinkcmf/cmf-app/src/portal/controller/IndexController.php
@@ -18,7 +18,6 @@ class IndexController extends HomeBaseController
     // 首页
     public function index()
     {
-//        return $this->fetch(":index");
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
         $nonce = $_GET["nonce"];
@@ -28,20 +27,40 @@ class IndexController extends HomeBaseController
         sort($tmpArr, SORT_STRING);
         $tmpStr = implode( $tmpArr );
         $tmpStr = sha1( $tmpStr );
-
+        file_put_contents('../data/runtime/log/1.txt',json_encode($_REQUEST).PHP_EOL, FILE_APPEND);
         if( $tmpStr == $signature ){
-            file_put_contents('../data/runtime/log/1.txt',json_encode($_REQUEST).PHP_EOL, FILE_APPEND);
+            header('content-type:text');
             echo $_GET['echostr'];
+            file_put_contents('../data/runtime/log/1.txt',"success".PHP_EOL, FILE_APPEND);
+            return true;
         }else{
+            echo "failure";
+            file_put_contents('../data/runtime/log/1.txt',"failure".PHP_EOL, FILE_APPEND);
             return false;
         }
 
     }
 
 
-    public function checkSignature()
+    /**
+     * 用SHA1算法生成安全签名
+     * @param string $token 票据
+     * @param string $timestamp 时间戳
+     * @param string $nonce 随机字符串
+     * @param string $encrypt 密文消息
+     */
+    public function getSHA1($token, $timestamp, $nonce, $encrypt_msg)
     {
-
+        //排序
+        try {
+            $array = array($encrypt_msg, $token, $timestamp, $nonce);
+            sort($array, SORT_STRING);
+            $str = implode($array);
+            return array(ErrorCode::$OK, sha1($str));
+        } catch (Exception $e) {
+            //print $e . "\n";
+            return array(ErrorCode::$ComputeSignatureError, null);
+        }
     }
 
     public function wxToken()
