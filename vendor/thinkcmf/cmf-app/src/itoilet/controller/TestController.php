a710418a08adb21c91f7ef9f000206a8df4a006e
zhangpai <394980922@qq.com>
2021-04-15 17:19:46 +0800

    thinkcmf

--- vendor/thinkcmf/cmf-app/src/itoilet/controller/TestController.php
+++ vendor/thinkcmf/cmf-app/src/itoilet/controller/TestController.php
@@ -47,7 +47,6 @@ class TestController extends BaseController {
 
     public function getopen()
     {
-
       //  $callbackUrl = urlencode($this->apiUrl.url("test/saveopenid"));
         $callbackUrl = "https://mycmf.yirj.xin/itoilet/test/saveopenid";
         $codeUrl = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$this->appid."&redirect_uri=".$callbackUrl."&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect";
