d0ea4f96631df24efbcc5c65ae38418215d492d3
zhangpai <394980922@qq.com>
2021-04-22 10:04:02 +0800

    thinkcmf

---
+++ vendor/thinkcmf/cmf-app/src/itoilet/controller/AdminIndexController.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zw
+ * Date: 2021/4/12
+ * Time: 14:49
+ * 智能马桶
+ */
+namespace app\itoilet\controller;
+
+use think\Db;
+use cmf\controller\AdminBaseController;
+
+class AdminIndexController extends AdminBaseController
+{
+    public function _empty()
+    {
+//        dump($this);
+//        $view = $this->view;
+//        dump($view);
+        $config_template = config();
+        $viewBase = config('template.view_base');
+        $viewSuffix = config('template.view_suffix');
+      //  dump($this->request);
+        $pathinfo = $this->request->pathinfo();
+        $tpl = $viewBase.$pathinfo.'.'.$viewSuffix;
+        $tpl = str_replace('\\','/',$tpl);
+        if(is_file($tpl))
+        {
+            return $this->fetch();
+        }
+        else
+        {
+            echo 'hehe';
+        }
+        
+    }
+//    public function index()
+//    {
+//       return $this->fetch();
+//    }
+
+
+    //-- 产品录入
+    public function record()
+    {
+        dump($this->request->param());
+    }
+}
